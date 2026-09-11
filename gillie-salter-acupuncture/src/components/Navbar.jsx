3656538058ae283f5e4655d958a5559726b80da6
danielsalter <danielcallumsalter@outlook.com>
2026-09-11 18:04:36 +0100

    initliased write version of vite (react + js), and created placeholder navbar and home page.

---
+++ gillie-salter-acupuncture/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+    return (
+        <nav>
+            <h2>Gillie Salter Acupuncture</h2>
+
+            <div>
+                <a href="/">Home</a>
+                <a></a>
+                <a href="/about">About</a>
+                <a href="/acupuncture">Acupuncture</a>
+                <a href="/fees">Fees</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ gillie-salter-acupuncture/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+function Home() {
+    return (
+        <main>
+
+            <section>
+                <h1>Gillie Salter Acupuncture</h1>
+
+                <p>
+                    Professional acupuncture treatment
+                    in St Neots.
+                </p>
+
+                <button>
+                    Book an appointment
+                </button>
+            </section>
+
+        </main>
+    );
+}
+
+export default Home;
